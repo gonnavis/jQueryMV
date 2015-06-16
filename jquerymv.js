f8689c28108018e9294748a90526ed41a032a390
gonnavis <gonnavis@163.com>
2015-06-16 22:48:13 +0800

    Create jquerymv.js

---
+++ jquerymv.js
@@ -0,0 +1,42 @@
+var MV = {};
+MV.bindInput = function(varName,value,selector){
+	if(!MV[varName]){
+		Object.defineProperty(MV, varName, {
+			get: function(){
+				return this['_'+varName];
+			},
+			set: function(newValue){
+				this['_'+varName] = newValue;
+				$(selector).val(newValue);
+				$(selector).html(newValue);
+			}
+		});
+	}
+	MV[varName] = value;
+	MV['_'+varName] = value;
+}
+
+$.fn.extend({
+	mvBind: function(varName){
+		var selector = this.selector;
+		$.each(this,function(){
+			if(this.tagName === 'INPUT'){
+				this.addEventListener('keyup',function(){
+					MV.bindInput(varName,this.value,selector);
+				});
+				this.addEventListener('cut',function(){
+					var elem = this;
+					setTimeout(function(){
+						MV.bindInput(varName,elem.value,selector);
+					},4);
+				});
+				this.addEventListener('paste',function(){
+					var elem = this;
+					setTimeout(function(){
+						MV.bindInput(varName,elem.value,selector);
+					},4);
+				});
+			}
+		});
+	}
+});
