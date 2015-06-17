7f1a4cc31c9266dbff6d0e61b0638ac1833a98a4
gonnavis <gonnavis@163.com>
2015-06-17 10:26:02 +0800

    Update jquerymv.js

--- jquerymv.js
+++ jquerymv.js
@@ -1,17 +1,5 @@
 var MV = {};
 MV.bindInput = function(varName,value,selector){
-	if(!MV[varName]){
-		Object.defineProperty(MV, varName, {
-			get: function(){
-				return this['_'+varName];
-			},
-			set: function(newValue){
-				this['_'+varName] = newValue;
-				$(selector).val(newValue);
-				$(selector).html(newValue);
-			}
-		});
-	}
 	MV[varName] = value;
 	MV['_'+varName] = value;
 }
@@ -19,6 +7,20 @@ MV.bindInput = function(varName,value,selector){
 $.fn.extend({
 	mvBind: function(varName){
 		var selector = this.selector;
+
+		if(!MV[varName]){
+			Object.defineProperty(MV, varName, {
+				get: function(){
+					return this['_'+varName];
+				},
+				set: function(newValue){
+					this['_'+varName] = newValue;
+					$(selector).val(newValue);
+					$(selector).html(newValue);
+				}
+			});
+		}
+
 		$.each(this,function(){
 			if(this.tagName === 'INPUT'){
 				this.addEventListener('keyup',function(){
